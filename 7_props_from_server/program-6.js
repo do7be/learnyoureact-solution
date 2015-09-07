fa11579785012b460bf350446555d32e82bd518d
do7be <bomsap700@gmail.com>
2015-09-08 00:58:01 +0900

    add lesson 7 props from server with es6

---
+++ 7_props_from_server/program-6.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+
+app.set('port', (process.argv[2] || 3000));
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine());
+
+require('node-jsx').install();
+
+let data = [
+  {title: "Shopping", detail: process.argv[3]},
+  {title: "Hair cut", detail: process.argv[4]}
+];
+
+app.use('/', (req, res) => {
+  res.render('index', {data: data});
+});
+
+app.listen(app.get('port'), () => {});
